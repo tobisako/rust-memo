4ee66a917f72c90a16b80202e39dd677824b3b4c
tobisakomd <tobisako@minden.co.jp>
2020-08-19 08:59:32 +0900

    add trait2

--- basic/trait/trait1/src/main.rs
+++ basic/trait/trait1/src/main.rs
@@ -1,8 +1,21 @@
 
+// トレイト内の「関連型」と「ジェネリック」の違い
+// 
+
+// 「ジェネリック」例：この様にジェネリクスを使用すると、各実装で型を注釈しなければならない
+// pub trait Iterator<T> {
+//     fn next(&mut self) -> Option<T>;
+// }
+
+// 「関連型」例：同じ型に対してトレイトを複数回実装できないので、型を注釈する必要はありません。
+// Counterにnextを呼び出す度に、 u32値のイテレータが欲しいと指定しなくてもよいわけです。
+// このtypeでは、１回しか型を選択（指定）しないで以後使い続ける事ができて便利。
 pub trait Iterator {
     type Item;
     fn next(&mut self) -> Option<Self::Item>;
 }
+
+// ※後ほど動作サンプルをここに持ってくる。
 
 fn main() {
     println!("Hello, world!");
